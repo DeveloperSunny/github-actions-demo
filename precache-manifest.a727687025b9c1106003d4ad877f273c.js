38fb8a113d4b0dadc8cb0fd693413acb6e927b9a
DeveloperSunny <developer_sunny@sina.com>
2020-01-07 07:10:36 +0000

    Deploying to gh-pages from master f07c78c7ffda17d9193e24733cd3bae6e78a6d39

---
+++ precache-manifest.a727687025b9c1106003d4ad877f273c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab8b94cf90036415e160f6f2b1259b4",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "10bfc457f2b7f009e08d",
+    "url": "/github-actions-demo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "86f6c9c4fae31fb854ec",
+    "url": "/github-actions-demo/static/js/2.6baffd64.chunk.js"
+  },
+  {
+    "revision": "10bfc457f2b7f009e08d",
+    "url": "/github-actions-demo/static/js/main.0c7ab6ae.chunk.js"
+  },
+  {
+    "revision": "26de67b8817671e67311",
+    "url": "/github-actions-demo/static/js/runtime~main.0777da02.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-actions-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
